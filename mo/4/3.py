581fdf96bedbb7b98ab733548c5a0fc00142e2c7
KlyukinSA <ctepa.kl@mail.ru>
2024-04-12 13:50:23 +0300

    mo4: fix test-train 'Embarked' and **'sex'** fields

--- mo/4/3.py
+++ mo/4/3.py
@@ -8,6 +8,7 @@ import pandas as pn
 
 # готовим тренировочные данные
 dataset_train = pn.read_csv('titanic_train.csv', sep=',')
+# print(dataset_train.iloc[0])
 dataset_train = dataset_train.drop(columns='Name')
 dataset_train = dataset_train.drop(columns='Ticket')
 dataset_train = dataset_train.drop(columns='Cabin')
@@ -19,35 +20,35 @@ dataset_train = dataset_train.drop(columns='Sex')
 dataset_train.loc[dataset_train['Embarked'] == 'Q', 'Embarked'] = 0
 dataset_train.loc[dataset_train['Embarked'] == 'S', 'Embarked'] = 1
 dataset_train.loc[dataset_train['Embarked'] == 'C', 'Embarked'] = 2
-dataset_train = dataset_train.drop(columns='Embarked')
 dataset_train_answer = dataset_train['Pclass']
 dataset_train = dataset_train.drop(columns='Pclass')
 imputer = KNNImputer(n_neighbors=3)
 dataset_train = imputer.fit_transform(dataset_train)
 
-#готовим тестовые данные
+# готовим тестовые данные
 dataset_test = pn.read_csv('titanic_test.csv', sep=',')
 dataset_test = dataset_test.drop(columns='Name')
 dataset_test = dataset_test.drop(columns='Ticket')
 dataset_test = dataset_test.drop(columns='Cabin')
 dataset_test = dataset_test.drop(columns='PassengerId')
-dataset_test.loc[dataset_test['Sex'] == 'female', 'Sex'] = 0
-dataset_test.loc[dataset_test['Sex'] == 'male', 'Sex'] = 1
+dataset_test.loc[dataset_test['Sex'] == 'female', 'sex'] = 0
+dataset_test.loc[dataset_test['Sex'] == 'male', 'sex'] = 1
 dataset_test = dataset_test.drop(columns='Sex')
 dataset_test.loc[dataset_test['Embarked'] == 'Q', 'Embarked'] = 0
 dataset_test.loc[dataset_test['Embarked'] == 'S', 'Embarked'] = 1
 dataset_test.loc[dataset_test['Embarked'] == 'C', 'Embarked'] = 2
-# dataset_test = dataset_test.drop(columns='Embarked')
 dataset_test_answer = dataset_test['Pclass']
 dataset_test = dataset_test.drop(columns='Pclass')
 imputer = KNNImputer(n_neighbors=3)
 dataset_test = imputer.fit_transform(dataset_test)
 
 # строим классификатор
-one_classifiers = [("descision tree", DecisionTreeClassifier()),
-               ("gaussian nb", GaussianNB()),
-               ("SVC", SVC()),
-               ("k-neighbors", KNeighborsClassifier())]
+one_classifiers = [
+    ("descision tree", DecisionTreeClassifier()),
+    ("gaussian nb", GaussianNB()),
+    ("SVC", SVC()),
+    ("k-neighbors", KNeighborsClassifier())
+]
 titles = ("descision tree", "gaussian nb", "SVC", "k-neighbors")
 
 for m, title in zip(one_classifiers, titles):
