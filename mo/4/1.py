8c37b680aaa83ed78943550ad5ce3008d21d6d6a
KlyukinSA <ctepa.kl@mail.ru>
2024-04-12 13:24:20 +0300

    mo 4: fix tasks 1 2 add train_test_split

--- mo/4/1.py
+++ mo/4/1.py
@@ -5,18 +5,19 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+dataset = pn.read_csv('glass.csv', sep=',').to_numpy()
+x_train, x_test, y_train, y_test = train_test_split(dataset[:, 1:-1], dataset[:, -1], train_size=0.8)
 
 classifiers = (DecisionTreeClassifier(), GaussianNB(), KNeighborsClassifier(), SVC())
 titles = ("decision tree", "gaussian nb", "K-neighbors", "SVC")
-dataset = pn.read_csv('glass.csv', sep=',').to_numpy()[:, 1:-1]
-dataset_answer = pn.read_csv('glass.csv', sep=',').to_numpy()[:, -1]
 for m, title in zip(classifiers, titles):
     accuracy = []
     for i in range(1, 101):
-        print(i)
         bagging = BaggingClassifier(estimator=m, n_estimators=i)
-        bagging.fit(dataset, dataset_answer)
-        accuracy.append(bagging.score(dataset, dataset_answer))
+        bagging.fit(x_train, y_train)
+        accuracy.append(bagging.score(x_test, y_test))
     plt.title(title)
     plt.xlabel("classifiers count")
     plt.ylabel("accuracy")
--- mo/4/2.py
+++ mo/4/2.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 import pandas as pn
 import numpy as np
@@ -8,28 +9,38 @@ import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
-answer = {'van': 0,
-          'saab': 1,
-          'bus': 2,
-          'opel': 3}
-dataset = pn.read_csv('vehicle.csv', sep=',').to_numpy()[:, :-1]
-dataset_answer = np.array(pn.read_csv('vehicle.csv', sep=','))[:, -1]
-for i in range(len(dataset_answer)):
-    dataset_answer[i] = answer[dataset_answer[i]]
-dataset_answer = dataset_answer.astype(int)
-classifiers = (SVC(probability=True), DecisionTreeClassifier(), GaussianNB(), )
+dataset = pn.read_csv('vehicle.csv', sep=',').to_numpy()
+X = dataset[:, :-1]
+Y = dataset[:, -1]
+
+answer = {
+    'van': 0,
+    'saab': 1,
+    'bus': 2,
+    'opel': 3
+}
+for i in range(len(Y)):
+    Y[i] = answer[Y[i]]
+Y = Y.astype(int)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.8)
+
+classifiers = (SVC(probability=True), DecisionTreeClassifier(), GaussianNB())
 titles = ("SVC", "decision tree", "gaussian nb")
-for m, title in zip(classifiers, titles):
+start = 1
+for m, title in zip(classifiers[start:], titles[start:]):
     accuracy = []
-    for i in range(1, 10):
-        print(i)
+    n = [i for i in range(1, 101, 1)]
+    for i in n:
+        if i % 51 == 0:
+            print(i)
         adaBoost = AdaBoostClassifier(estimator=m, n_estimators=i)
-        adaBoost.fit(dataset, dataset_answer)
-        accuracy.append(adaBoost.score(dataset, dataset_answer))
+        adaBoost.fit(x_train, y_train)
+        accuracy.append(adaBoost.score(x_test, y_test))
     plt.title(title)
     plt.xlabel("classifiers count")
     plt.ylabel("accyracy")
-    plt.plot(accuracy)
+    plt.plot(n, accuracy)
     plt.show()
 
 print()
